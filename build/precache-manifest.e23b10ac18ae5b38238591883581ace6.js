de6fb8fa40a5a538b3953d118da37f72b24df518
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-11-19 13:08:51 -0600

    added npm run build

---
+++ build/precache-manifest.e23b10ac18ae5b38238591883581ace6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c7ab6c9c7be93403cceafef7a8c1da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d6979692cff0c623f0c",
+    "url": "/static/css/main.a1f3e13c.chunk.css"
+  },
+  {
+    "revision": "1aca27c44fb8ed468bf8",
+    "url": "/static/js/2.ea93de27.chunk.js"
+  },
+  {
+    "revision": "e72aa5db2c8ada42b45ae4ae98923678",
+    "url": "/static/js/2.ea93de27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d6979692cff0c623f0c",
+    "url": "/static/js/main.b2bfd15a.chunk.js"
+  },
+  {
+    "revision": "80d5dc00158f2545199a",
+    "url": "/static/js/runtime-main.9a94cc45.js"
+  }
+]);
