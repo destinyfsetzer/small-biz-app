de56c1ddda893b0affdcf47d3d8cdc202daf54ab
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-11-19 12:59:14 -0600

    testing build limit

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -9,7 +9,7 @@ const Details = (props) => {
   return (
     <Container maxWidth="sm" className="car-container">
       <Typography className="car-paper">
-        <h2>{bizz.name}</h2>
+        <h2>HALP ME{bizz.name}</h2>
         <h4>{bizz.address}</h4>
         <h4>{bizz.hours}</h4>
         <p>{bizz.description}</p>
